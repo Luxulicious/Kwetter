f1768313cf98dc3084f101cfd6f0565a230da509
Ruijs,Tom T.J <tom.ruijs@student.fontys.nl>
2018-03-09 16:26:13 +0100

    Initial role rest layer done

--- Kwetter/src/main/java/boundary/rest/RoleResource.java
+++ Kwetter/src/main/java/boundary/rest/RoleResource.java
@@ -5,10 +5,16 @@
  */
 package boundary.rest;
 
+import boundary.rest.response.GetMultipleResponse;
+import boundary.rest.response.UpdateResponse;
+import domain.Role;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import service.RoleService;
+import service.exceptions.NonExistingRoleException;
+import service.exceptions.NonExistingUserException;
 
 /**
  *
@@ -22,4 +28,26 @@ public class RoleResource {
 
     @Inject
     RoleService roleService;
+
+    @Path("getAllRoles")
+    public GetMultipleResponse<Role> getAllRoles() {
+        GetMultipleResponse<Role> response = new GetMultipleResponse<>();
+        response.setRecords(roleService.getAllRoles());
+        response.setSucces(true);
+        return response;
+    }
+
+    @Path("setUserRole/{roleName}/{userId}")
+    public UpdateResponse<Role> setUserRole(@PathParam("roleName") String roleName, @PathParam("userId") Long userId) {
+        UpdateResponse<Role> response = new UpdateResponse<>();
+        try {
+            roleService.setUserRole(roleName, userId);
+            response.setSucces(true);
+        } catch (NonExistingRoleException ex) {
+            response.addMessage("De rol " + roleName + " bestaat niet.");
+        } catch (NonExistingUserException ex) {
+            response.addMessage("Deze gebruiker bestaat niet.");
+        }
+        return response;
+    }
 }
