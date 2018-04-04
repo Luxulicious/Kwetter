6260386721e20b1f16fe49fe2e147c530360f95c
Ruijs,Tom T.J <tom.ruijs@student.fontys.nl>
2018-04-04 10:41:23 +0200

    Messing around with components

--- Kwetter-App/src/app/app.module.ts
+++ Kwetter-App/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { ProfileNameComponent } from './profile-name/profile-name.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfilePageComponent,
-    HomePageComponent, 
+    HomePageComponent,
+    ProfileNameComponent, 
   ],
   imports: [
     BrowserModule,
---
+++ Kwetter-App/src/app/models/user.ts
@@ -0,0 +1,7 @@
+export class User {
+    username: string;
+    bio: string;
+    location: string;
+    website: string;
+    icon: string;
+}
