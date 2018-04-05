b7146e5df20de1098f345f4f626ee3ae8a75389e
Ruijs,Tom T.J. <tom.ruijs@student.fontys.nl>
2018-04-05 21:40:44 +0200

    Setting up authentication components

--- Kwetter-App/src/app/app-routing.module.ts
+++ Kwetter-App/src/app/app-routing.module.ts
@@ -2,11 +2,25 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ProfileComponent} from './profile/profile.component';
 import {HomeComponent} from './home/home.component';
+import {SignInComponent} from './authentication/sign-in/sign-in.component';
+import {AuthenticationComponent} from './authentication/authentication.component';
+import {SignUpComponent} from './authentication/sign-up/sign-up.component';
+
 
 
 const routes: Routes = [
     {path: 'profile', component: ProfileComponent},
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {
+        path: 'sign-up', component: AuthenticationComponent,
+        children: [{path: '', component: SignUpComponent}]
+    },
+    {
+        path: 'sign-in', component: AuthenticationComponent,
+        children: [{path: '', component: SignInComponent}]
+    },
+    { path: '', redirectTo:'/sign-in', pathMatch: 'full'}
+
 ];
 
 @NgModule({
--- Kwetter-App/src/app/app.module.ts
+++ Kwetter-App/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ProfileNameComponent } from './profile-name/profile-name.component';
+import {AuthenticationComponent} from './authentication/authentication.component';
+import {SignInComponent} from './authentication/sign-in/sign-in.component';
+import {SignUpComponent} from './authentication/sign-up/sign-up.component';
 
 
 @NgModule({
@@ -15,6 +18,9 @@ import { ProfileNameComponent } from './profile-name/profile-name.component';
     ProfileComponent,
     HomeComponent,
     ProfileNameComponent,
+    AuthenticationComponent,
+    SignInComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
