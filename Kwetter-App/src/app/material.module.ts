4750a1b4e8711dddce42965d4e9789da53fe5b2c
Ruijs,Tom T.J <tom.ruijs@student.fontys.nl>
2018-04-09 14:17:00 +0200

    Forms made
    Change the router thing to tabs next

--- Kwetter-App/src/app/material.module.ts
+++ Kwetter-App/src/app/material.module.ts
@@ -1,10 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatButtonModule} from '@angular/material'; 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatDialogModule} from '@angular/material/dialog';
+
 
 @NgModule({
-    imports: [MatButtonModule],
-    exports: [MatButtonModule]
+    imports: [MatDialogModule, MatInputModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatFormFieldModule],
+    exports: [MatDialogModule, MatInputModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatFormFieldModule]
 })
 
 export class MaterialModule {}
